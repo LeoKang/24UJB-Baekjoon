adaf3ec62a1b62c1d8cdd24e132d3b5ea3800b73
manic-066g <xeungjin@nate.com>
2024-04-03 17:33:30 +0900

    2439

---
+++ Baekjoon/src/q2439/Main.java
@@ -0,0 +1,26 @@
+package q2439;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+
+		System.out.println(N);
+
+		for (int i = 0; i < N; i++) {
+			// ' ' 출력
+			for (int j = N - i - 1; j > 0; j--) {
+				System.out.print("?");
+			}
+			// '*' 출력
+			for (int k = 0; k <= i; k++) {
+				System.out.print("*");
+			}
+			// println()
+			System.out.println();
+		}
+	}
+}
